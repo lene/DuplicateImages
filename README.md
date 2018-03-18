9a92f468d23da39019fb169ba60477ae7b77cf5b
Lene Preuss <lene.preuss@gmail.com>
2018-03-18 15:48:04 +0100

    use mypy and flake8 to make code type safe and standards compliant

--- README.md
+++ README.md
@@ -3,4 +3,24 @@ DuplicateImages
 
 Finds equal or similar images in a directory containing (many) image files.
 
-Needs Python3, and PythonMagick to run.
+Needs Python3 and Pillow imaging library to run, additionally PythonMagick for test suite.
+
+Running
+-------
+```bash
+$ python3 duplicate.py $PICTURE_DIR
+```
+or
+```bash
+$ python3 duplicate.py -h
+```
+for a list of all possible options.
+
+Testing
+-------
+```bash
+$ mypy --strict --no-warn-return-any --ignore-missing-imports .
+$ flake8 .
+$ python3 test_duplicate.py 
+
+```
--- image_wrapper.py
+++ image_wrapper.py
@@ -2,6 +2,7 @@ __author__ = 'lene'
 
 from math import fabs
 from os.path import isfile, islink
+from typing import Dict, List, Optional, Tuple
 from PIL import Image
 
 
@@ -10,38 +11,38 @@ class ImageWrapper:
     Utility functions for image files, with optimizations for certain expensive functions
     """
 
-    cache = {}
+    cache = {}  # type: Dict[str, 'ImageWrapper']
 
     @classmethod
-    def create(cls, filename):
+    def create(cls, filename: str) -> 'ImageWrapper':
         """Create an ImageWrapper; if it was already created, return existing object"""
         if filename not in cls.cache:
             cls.cache[filename] = ImageWrapper(filename)
 
         return cls.cache[filename]
 
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.size = Image.open(self.filename).size
-        self.histogram = None
+        self.size = Image.open(self.filename).size  # type: Tuple[int, int]
+        self.histogram = None  # type: Optional[List[float]]
 
-    def get_area(self):
+    def get_area(self) -> int:
         return self.size[0] * self.size[1]
 
-    def get_aspect(self):
+    def get_aspect(self) -> float:
         return self.size[0] / self.size[1]
 
-    def get_histogram(self):
+    def get_histogram(self) -> List[float]:
         """Returns the histogram of the image file, every value normalized to [0..1]"""
         if self.histogram is None:
             self.histogram = list(map(
-                lambda v: v/self.get_area(),
+                lambda v: v / self.get_area(),
                 Image.open(self.filename).convert("RGB").histogram()
             ))
         return self.histogram
 
     @classmethod
-    def is_image_file(cls, filename):
+    def is_image_file(cls, filename: str) -> bool:
         """Returns True if filename is an image file"""
         try:
             if isfile(filename) and not islink(filename):
@@ -51,16 +52,15 @@ class ImageWrapper:
             return False
 
 
-def get_aspect_ratio(image, other_image):
+def get_aspect_ratio(image: ImageWrapper, other_image: ImageWrapper) -> float:
     return image.get_aspect() / other_image.get_aspect()
 
 
-def aspects_roughly_equal(image, other_image):
-    return fabs(get_aspect_ratio(image, other_image) - 1) < aspects_roughly_equal.FUZZINESS
+def aspects_roughly_equal(
+        image: ImageWrapper, other_image: ImageWrapper, aspect_fuzziness: float
+) -> float:
+    return fabs(get_aspect_ratio(image, other_image) - 1) < aspect_fuzziness
 
 
-aspects_roughly_equal.FUZZINESS = 0.05
-
-
-def min_image_size(image, other_image):
+def min_image_size(image: ImageWrapper, other_image: ImageWrapper) -> int:
     return min(image.get_area(), other_image.get_area())
